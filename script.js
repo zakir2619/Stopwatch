e09c3bf0c81f97a9f598b7937c4e62c1b55cbd1c
Md. Zakir Hossain <38485255+zakir2619@users.noreply.github.com>
2026-02-13 18:30:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+let startTime;
+let elapsedTime = 0;
+let timerInterval;
+
+function timeToString(time) {
+    let diffInHrs = time / 3600000;
+    let hh = Math.floor(diffInHrs);
+
+    let diffInMin = (diffInHrs - hh) * 60;
+    let mm = Math.floor(diffInMin);
+
+    let diffInSec = (diffInMin - mm) * 60;
+    let ss = Math.floor(diffInSec);
+
+    let diffInMs = (diffInSec - ss) * 100;
+    let ms = Math.floor(diffInMs);
+
+    let formattedMM = mm.toString().padStart(2, "0");
+    let formattedSS = ss.toString().padStart(2, "0");
+    let formattedMS = ms.toString().padStart(2, "0");
+    let formattedHH = hh.toString().padStart(2, "0");
+
+    return `${formattedHH}:${formattedMM}:${formattedSS}.${formattedMS}`;
+}
+
+function startStop() {
+    const btn = document.getElementById("startStopBtn");
+    
+    if (btn.innerHTML === "Start") {
+        startTime = Date.now() - elapsedTime;
+        timerInterval = setInterval(function printTime() {
+            elapsedTime = Date.now() - startTime;
+            document.getElementById("display").innerHTML = timeToString(elapsedTime);
+        }, 10);
+        btn.innerHTML = "Stop";
+        btn.style.backgroundColor = "#ff9800";
+    } else {
+        clearInterval(timerInterval);
+        btn.innerHTML = "Start";
+        btn.style.backgroundColor = "#4CAF50";
+    }
+}
+
+function reset() {
+    clearInterval(timerInterval);
+    document.getElementById("display").innerHTML = "00:00:00.00";
+    elapsedTime = 0;
+    const btn = document.getElementById("startStopBtn");
+    btn.innerHTML = "Start";
+    btn.style.backgroundColor = "#4CAF50";
+}
